07cb09fbf06f4e3ddc5af0300556e6f5920a092d
Eike Gebauer <e_geba02@wwu.de>
2018-04-26 13:49:01 +0200

    Added half edge model loading draft.

--- assignment1/half_edge_mesh.cpp
+++ assignment1/half_edge_mesh.cpp
@@ -8,6 +8,14 @@ namespace cg
 {
 	HalfEdgeMesh::HalfEdgeMesh(std::string file_path)
 	{
-		
+		auto importer = Assimp::Importer;
+		const aiScene* scene = importer.ReadFile(file_path, aiProcess_JoinIdenticalVertices);
+
+		if(!scene)
+		{
+			std::cerr << "Assimp failed to read file " << file_path << " with message: " << importer.GetErrorString() << '\n';
+			throw std::runtime_error{"Assimp failed to load file."};
+		}
+
 	}
 }
